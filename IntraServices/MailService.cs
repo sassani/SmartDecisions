52a1e498b39fa2219b6003cdfd7391f82e98d823
Ardavan <a.sassani@gmail.com>
2020-01-29 17:46:38 -0500

    Add IntraServices

---
+++ IntraServices/MailService.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntraServices
+{
+    public class MailService
+    {
+        private static readonly HttpClient client = new HttpClient();
+        public MailService(string apikey)
+        {
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            client.DefaultRequestHeaders.Add("apiKey", apikey);
+            client.BaseAddress = new Uri("https://mailservice.ardavansassani.com");
+        }
+        public async Task SendVerificationEmail(string email, string token)
+        {
+            try
+            {
+                Object data = new
+                {
+                    email,
+                    token
+                };
+                var json = JsonConvert.SerializeObject(data);
+                var stringContent = new StringContent(json, UnicodeEncoding.UTF8, "application/json");
+                HttpResponseMessage response = await client.PostAsync("email", stringContent);
+            }
+            catch (HttpRequestException e)
+            {
+
+                throw;
+            }
+        }
+    }
+}
