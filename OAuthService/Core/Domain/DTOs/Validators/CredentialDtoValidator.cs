93286b76531a783a1da05d251869991e21c19939
Ardavan <a.sassani@gmail.com>
2020-02-07 10:35:13 -0500

    update credential validator

--- OAuthService/Core/Domain/DTOs/Validators/CredentialDtoValidator.cs
+++ OAuthService/Core/Domain/DTOs/Validators/CredentialDtoValidator.cs
@@ -8,23 +8,26 @@ namespace OAuthService.Core.Domain.DTOs.Validators
         {
             string txt = " field is required";
 
-            RuleFor(cr => cr.GrantType).NotNull().WithMessage($"grantType{txt}");
+            RuleFor(cr => cr.GrantType).NotNull().WithMessage($"grantType{txt}").DependentRules(() =>
+            {
+                RuleFor(cr => cr.ClientId).NotNull()
+                    .When(cr => cr.GrantType.ToLower() == "idtoken").WithMessage($"clientId{txt}");
 
-            RuleFor(cr => cr.ClientId).NotEmpty()
-                .When(cr => cr.GrantType.ToLower() == "idtoken").WithMessage($"clientId{txt}");
+                RuleFor(cr => cr.Email).NotNull()
+                    .When(cr => cr.GrantType.ToLower() == "idtoken" || cr.GrantType.ToLower() == "register").WithMessage($"email{txt}").EmailAddress();
 
-            RuleFor(cr => cr.Email).NotNull()
-                .When(cr => cr.GrantType.ToLower() == "idtoken" || cr.GrantType.ToLower() == "register" || cr.GrantType.ToLower() == "forgotpassword").WithMessage($"email{txt}").EmailAddress();
+                RuleFor(cr => cr.Password).NotNull()
+                    .When(cr => cr.GrantType.ToLower() == "idtoken" || cr.GrantType.ToLower() == "register" || cr.GrantType.ToLower() == "forgotpassword").WithMessage($"password{txt}").MinimumLength(8).WithMessage("password must have at least 8 caracters");
 
-            RuleFor(cr => cr.Password).NotNull()
-                .When(cr => cr.GrantType.ToLower() == "idtoken" || cr.GrantType.ToLower() == "register").WithMessage($"password{txt}").MinimumLength(8).WithMessage("password must have at least 8 caracters");
+                RuleFor(cr => cr.RefreshToken).NotNull()
+                    .When(cr => cr.GrantType.ToLower() == "refreshtoken").WithMessage($"refreshtoken{txt}");
 
-            RuleFor(cr => cr.RefreshToken).NotNull()
-                .When(cr => cr.GrantType.ToLower() == "refreshtoken").WithMessage($"refreshtoken{txt}");
+                RuleFor(cr => cr.ClientSecret).NotNull()
+                    .When(cr => cr.GrantType.ToLower() == "idtoken" && cr.ClientId.ToLower() == "2").WithMessage($"clientsecret{txt} when you call it from mobile");
 
-            RuleFor(cr => cr.ClientSecret).NotEmpty()
-                .When(cr => cr.GrantType.ToLower() == "idtoken" && cr.ClientId.ToLower() == "2").WithMessage($"clientsecret{txt} when you call it from mobile");
-
+                RuleFor(cr => cr.ResetPasswordToken).NotNull()
+                    .When(cr => cr.GrantType.ToLower() == "forgotpassword").WithMessage($"resetpasswordtoken{txt}");
+            });
         }
     }
 }
