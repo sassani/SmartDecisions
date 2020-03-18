2a65f93262eef9e913ddaec03be0ab8f17fbe7d9
Ardavan <a.sassani@gmail.com>
2020-03-18 10:15:06 -0400

    Add secure test info controller

--- OAuthService/Controllers/InfoController.cs
+++ OAuthService/Controllers/InfoController.cs
@@ -3,13 +3,14 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System.Net;
+using System.IO;
 
 namespace OAuthService.Controllers
 {
-	[AllowAnonymous]
-	[Route("info")]
-	[ApiController]
-	public class InfoController : ControllerBase
+    [Authorize]
+    [Route("info")]
+    [ApiController]
+    public class InfoController : ControllerBase
     {
         private readonly ILogger<InfoController> _logger;
         public InfoController(ILogger<InfoController> logger)
@@ -17,14 +18,25 @@ namespace OAuthService.Controllers
             _logger = logger;
         }
 
+        [AllowAnonymous]
         [HttpGet()]
-		public IActionResult GetInfo()
-		{
-			var payload = new
-			{
-				Data = "This is initial data"
-			};
-			return new Response(HttpStatusCode.Accepted, payload).ToActionResult();
-		}
-	}
+        public IActionResult GetInfo()
+        {
+            var payload = new
+            {
+                Message = "This is initial data"
+            };
+            return new Response(HttpStatusCode.Accepted, payload).ToActionResult();
+        }
+
+        [HttpGet("secure")]
+        public IActionResult GetInfoSecure()
+        {
+            var payload = new
+            {
+                Message = "This is initial secure data"
+            };
+            return new Response(HttpStatusCode.Accepted, payload).ToActionResult();
+        }
+    }
 }
